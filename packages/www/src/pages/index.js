34911ca534e79111cf113d3262c5efbb559fffb4
Jesus Flores <me@jesusfloressanjose.com>
2020-09-27 15:32:09 +0200

    Add theme

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,18 @@
 import React from "react";
+import {Container, Heading, Button, Flex} from "theme-ui";
 
 export default props => (
-    <div>
-        <h1>Our site</h1>
-    </div>
+    <Container>
+        <Flex sx={{ flexDirection: "column" }}>
+            <Heading as="h1">Organiza tus listas</Heading>
+            <Button 
+            sx={{ marginTop: 2}}
+            onClick={() => {
+                alert("Clicked");
+            }}
+            >
+                Log In
+                </Button>
+        </Flex>        
+    </Container>
 );
